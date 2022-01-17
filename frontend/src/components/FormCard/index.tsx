37a44df2a579215bff772b88fd95878924f040be
anadunning <anakpedrosa@hotmail.com>
2022-01-17 00:21:32 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,15 +1,18 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "types/movie";
 import { BASE_URL } from "utils/requests";
 import "./styles.css";
+import { validateEmail } from "utils/validate";
 
 type Props = {
   movieId: string;
 };
 
 function FormCard({ movieId }: Props) {
+
+  const navigate = useNavigate();
 
   const [movie, setMovie] = useState<Movie>();
 
@@ -18,6 +21,33 @@ function FormCard({ movieId }: Props) {
       setMovie(response.data);
     });
   }, [movieId]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+    event.preventDefault();
+
+    const email = (event.target as any).email.value;
+    const score = (event.target as any).score.value;
+
+    if (!validateEmail(email)) {
+        return;
+    }
+
+    const config: AxiosRequestConfig = {
+      baseURL: BASE_URL,
+      method: 'PUT',
+      url: '/scores',
+      data: {
+        email: email,
+        movieId: movieId,
+        score: score
+      }
+    }
+
+    axios(config).then(response => {
+      navigate("/");
+    });
+  }
 
   return (
     <div className="dsmovie-form-container">
